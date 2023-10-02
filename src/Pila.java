5caee959a100196cbe307f354d1144b490333526
ASUS <rikardoperez0809@gmail.com>
2023-10-02 17:43:25 -0500

    primer avance

---
+++ src/Pila.java
@@ -0,0 +1,31 @@
+import java.util.Stack;
+public class Pila {
+    private Stack<Integer> pila;
+
+    public Pila() {
+        pila=new Stack<Integer>();
+
+    }
+    public void insertar(Integer dato){
+        pila.push(dato);
+    }
+    public Integer eliminar() throws Exception {
+        if(pila.empty())
+            throw new Exception("Pila sin elementos");
+        return pila.pop();
+
+    }
+    public Integer consultarTope() throws Exception {
+        if (pila.empty())
+            throw new Exception("Pila sin elementos");
+        return pila.peek();
+    }
+
+    @Override
+    public String toString() {
+        String resltado= "===Pila===\n";
+        for (int i=pila.size()-1; i>=0; i--){
+        }
+        return resltado;
+    }
+}
